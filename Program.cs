c49e1e3b22fb9d4418382f5d6180e2919583076a
Icaka07 <stoychev07v03@gmail.com>
2026-02-12 11:09:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Mehmedimamalukkur;
+using System;
+
+namespace Mehmedimamalukkur
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            Person person = new Person();
+            person.Name = "Шивачко Шивачеш";
+            person.Age = 15;
+            person.IntroduceYourself();
+        }
+    }
+}
